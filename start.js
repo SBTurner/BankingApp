bb358ae6a89669c4cfba447910274003c0fbd297
SBTurner <sarah.turner@sky.uk>
2021-02-12 10:52:44 +0000

    changed app port

--- start.js
+++ start.js
@@ -1,6 +1,6 @@
 const app = require("./server")
 
-app.listen(3000, () =>
-  console.log("Running on localhost:3000")
-);
 
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Running app on port', process.env.PORT || 3000)
+})
